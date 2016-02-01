bcc71f3c400b1165cc28dc21d709c320330e34cd
Kyle N. Smith <smith.kylen@gmail.com>
2016-01-31 21:01:09 -0500

    added some user logic

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,8 +1,18 @@
 class UserController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+      @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+      flash[:failure]
+    end
   end
 
   def new
@@ -11,6 +21,24 @@ class UserController < ApplicationController
   def index
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def show
   end
 end
+
+
+
+
+  
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+
+
